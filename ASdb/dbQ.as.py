190bd1241bf98bb750a5d1e94a91272a299695e5
MarcusDechant <marcusdechant@gmail.com>
2022-08-21 13:36:32 -0600

    Updated dbQ

--- ASdb/dbQ.as.py
+++ ASdb/dbQ.as.py
@@ -2,12 +2,13 @@
 
 import sqlite3 as sql
 
+s=', '
+db=('aquasolar.db')
 
 while(True):
     try:
-        table=str.lower(input('\nEvents, Sun, or Rain?\n'))
+        table=str.lower(input('\nEvents, Sun, Rain, THS, or Pi Stats?\n'))
         
-        db=('aquasolar.db')
         database=sql.connect(db)
         
         if(table=='events'):
@@ -19,9 +20,9 @@ while(True):
                 r2=str(row[2])
                 r3=str(row[3])
                 r4=str(row[4])
-                s=', '
                 print(r0+s+r1+s+r2+s+r3+s+r4)
             break
+            
         elif(table=='sun'):
             cur=database.execute('''SELECT * FROM SUN''')
             print('\nDay, Sunrise, Sunset')
@@ -29,9 +30,9 @@ while(True):
                 r0=str(row[0])
                 r1=str(row[1])
                 r2=str(row[2])
-                s=', '
                 print(r0+s+r1+s+r2)
             break
+            
         elif(table=='rain'):
             cur=database.execute('''SELECT * FROM RAIN''')
             print('\nDay, Sunrise, Sunset')
@@ -40,12 +41,39 @@ while(True):
                 r1=str(row[1])
                 r2=str(row[2])
                 r3=str(row[3])
-                s=', '
                 print(r0+s+r1+s+r2+r3)
-            break    
+            break
+            
+        elif(table=='ths'):
+            cur=database.execute('''SELECT * FROM SENSOR1''')
+            print('\nID, Delay, Temp, Humi, Time, Date')
+            for row in cur:
+                r0=str(row[0])
+                r1=str(row[1])
+                r2=str(row[2])
+                r3=str(row[3])
+                r4=str(row[4])
+                r5=str(row[5])
+                print(r0+s+r1+s+r2+s+r3+s+r4+s+r5)
+            break
+            
+        elif(table=='pi_stats'):
+            cur=database.execute('''SELECT * FROM PI_STATS''')
+            print('\nID, Delay, CPUTemp, CPULoad, Time, Date')
+            for row in cur:
+                r0=str(row[0])
+                r1=str(row[1])
+                r2=str(row[2])
+                r3=str(row[3])
+                r4=str(row[4])
+                r5=str(row[5])
+                print(r0+s+r1+s+r2+s+r3+s+r4+s+r5)
+            break
+        
         else:
             print('error')
             pass
+            
     except KeyboardInterrupt:
         print('\n')
         exit(0)
